c07c9c4e6f37bd7bd94df611e4d0c324771ec591
Jhony Souza <jhonatansouzameza100@gmail.com>
2024-01-02 04:02:52 -0300

    020120240402

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 const HEADER = document.querySelector("header")
 window.addEventListener("scroll", () => {HEADER.classList.toggle("sticky", window.scrollY > 60)})
+const MENU = document.querySelector("#menu-icon")
+const NAVBAR = document.querySelector(".navbar")
+MENU.onclick = () => {
+  MENU.classList.toggle("bx-x")
+  NAVBAR.classList.toggle("open")
+}
