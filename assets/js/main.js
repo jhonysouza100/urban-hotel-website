226363dd4c253b650f9a3c367fe1866fc414ed6b
jhony <jhonatansouzameza100@gmail.com>
2024-01-17 08:58:46 -0300

    170120240859

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,9 +81,13 @@ let swiperFavorite = new Swiper('.home-slider', {
   slidesPerView: 'auto',
   centeredSlides: 'auto',
   grabCursor: true,
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
+  // autoplay: {
+  //   delay: 8000,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
   speed: 1500,
   ease: 'ease-in-out'
