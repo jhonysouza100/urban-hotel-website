44a8fa3bcb1a2da8fb2acc95f97101ef781db912
jhony <jhonatansouzameza100@gmail.com>
2024-02-17 20:43:43 -0300

    17020242043

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -82,7 +82,8 @@ function header() {
         {/* theme button */}
         <div className="theme-button" onClick={toggleTheme}>
           {/* <i className={`${icon}`} /> */}
-          {userData ? <Avatar alt="User" src={userData.picture} sx={{ width: 30, height: 30 }} /> : <img className="nav-logo" src={logoImg} alt="logo img" />}
+          {userData ? <Avatar alt="User" src={userData.picture} sx={{ width: 35, height: 35 }} /> : <img className="nav-logo" src={logoImg} alt="logo img" />}
+          {userData && <p>{userData.username}</p>}
         </div>
         <div className={`navmenu`} id="navmenu" onClick={handleClick}>
           <ul className="navmenu-list">
